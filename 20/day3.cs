f6934a42e3abfd3110992da50cf804ea3167a450
nils måsén <nils@piksel.se>
2020-12-05 09:40:19 +0100

    20: Add day 3

---
+++ 20/day3.cs
@@ -0,0 +1,10 @@
+using System.Linq;
+using static System.IO.File;
+using static System.Console;
+
+int GetTreeProd(string[] map, params (int x, int y)[] strats)
+    => strats.Aggregate(1, (a, t) => a * Enumerable.Range(0, map.Length / t.y).Count(i => map[t.y * i][t.x * i % map[0].Length] == '#'));
+
+var map = ReadAllLines("input.txt");
+WriteLine($"Silver: {GetTreeProd(map, (3, 1))}");
+WriteLine($"Gold: {GetTreeProd(map, (1,1), (3,1), (5,1), (7,1), (1,2))}");
