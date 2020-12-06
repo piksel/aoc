40c42815c353627bd442cb1f818a3eec9a70e1bf
nils måsén <nils@piksel.se>
2020-12-06 13:37:23 +0100

    20: add day 6

---
+++ 20/day6.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+using static System.Console;
+
+var answers = File.ReadAllText(args[0]).Split("\n\n")
+    .Select(g => g.Split('\n').Where(line => line.Length > 0));
+
+var groupYesAnswers = answers
+    .Select(g => string.Concat(string.Concat(g).Distinct()).Length)
+    .Sum();
+
+var groupAllYes = answers
+    .Select(g => g.SelectMany(p => p.ToArray()).Distinct())
+    .Zip(answers).Select(g => 
+        g.First
+            .Where(a => 
+                g.Second.All(ga => ga.Contains(a)))
+            .Count())
+    .Sum();
+
+WriteLine($"Silver: {groupYesAnswers}");
+WriteLine($"Gold: {groupAllYes}");
