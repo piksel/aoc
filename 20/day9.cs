fdf5d8cf4a3216c3b1ad8eddfb049cc2cf03255a
nils måsén <nils@piksel.se>
2020-12-09 22:31:31 +0100

    20: add day 9 (slow!)

---
+++ 20/day9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+const int prevCount = 25;
+var input = File.ReadAllLines(args[0]).Select(line => long.Parse(line));
+
+var naughty = input.Skip(prevCount).Where(
+    (n, i) =>
+    {
+        var prevs = input
+            .Skip(i)
+            .Take(prevCount);
+        return !prevs
+            .Any(a => prevs.Any(b => a != b && a + b == n));
+    }).First();
+
+WriteLine($"Silver: {naughty}");
+
+var inputLen = input.Count();
+var allPos = Enumerable.Range(0, inputLen);
+
+var weakness = allPos.SelectMany(a =>
+    allPos.Skip(a).Select(b => input.Skip(a).Take(b-a)).Where(nums => nums.Sum() == naughty)
+        .Select(nums => (nums.Min() + nums.Max()))).First();
+
+WriteLine($"Gold: {weakness}");
